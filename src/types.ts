f80d96c81aa79bba3d3e37fa39bb0305ca122e15
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-04-28 13:25:23 +0200

    start shaping the api

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,42 @@
+import type { StandardSchemaDictionary } from './standard-schema'
+
+/** @internal */
+type TRouteDynamicSegmentsDict = StandardSchemaDictionary
+
 // Public API types
 export type Awaitable<T> = T | Promise<T>
 
-export type CreateOptions = {
+export type CreateSafeRouteHandlerOptions<
+  TRouteDynamicSegments extends TRouteDynamicSegmentsDict,
+> = {
   /**
    * Name for the route handler.
    */
   name?: string
   /**
-   * Dynamic route params used in the route handler path.
+   * Dynamic route segments used in the route handler path.
    */
-  routeParams?: any
+  routeDynamicSegments?: TRouteDynamicSegments
 }
 
-export type Context<TRouteParams> = {
-  name: string
-  routeParams: TRouteParams
+export type SafeRouteHandlerContext<
+  TRouteDynamicSegments extends TRouteDynamicSegmentsDict,
+> = {
+  /**
+   * Route handler dynamic segments
+   */
+  routeDynamicSegments: StandardSchemaDictionary.InferOutput<TRouteDynamicSegments>
 }
+
+export type CreateSafeRouteHandlerReturnType = (
+  /**
+   * Original request
+   */
+  req: Request,
+  /**
+   * Extras added by Next.js itself
+   */
+  extras: {
+    params: Awaitable<Record<string, string | string[] | undefined>>
+  }
+) => Promise<Response>
