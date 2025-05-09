5b6f22b84efde115f35db0fc65730a987ea76e92
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-05-09 16:05:20 +0200

    scaffold mdx components

---
+++ www/mdx-components.tsx
@@ -0,0 +1,90 @@
+import type { MDXComponents } from 'mdx/types'
+import { cn } from '~/lib/utils'
+
+const components: MDXComponents = {
+  a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+    <a
+      className={cn('font-medium underline underline-offset-4', className)}
+      {...props}
+    />
+  ),
+  blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <blockquote
+      className={cn('mt-6 border-l-2 pl-6 italic', className)}
+      {...props}
+    />
+  ),
+  code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <code
+      className={cn(
+        'relative rounded px-[0.2rem] py-[0.2rem] font-mono text-sm',
+        className
+      )}
+      {...props}
+    />
+  ),
+  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h1
+      className={cn(
+        'font-heading mt-2 scroll-m-20 text-4xl font-bold',
+        className
+      )}
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h2
+      className={cn(
+        'font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0',
+        className
+      )}
+      {...props}
+    />
+  ),
+  h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h3
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
+        className
+      )}
+      {...props}
+    />
+  ),
+  h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h4
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+        className
+      )}
+      {...props}
+    />
+  ),
+  h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h5
+      className={cn(
+        'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+        className
+      )}
+      {...props}
+    />
+  ),
+  h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h6
+      className={cn(
+        'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
+        className
+      )}
+      {...props}
+    />
+  ),
+  hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
+    <hr className='my-4 md:my-8' {...props} />
+  ),
+}
+
+export function useMDXComponents(overrides: MDXComponents): MDXComponents {
+  return {
+    ...components,
+    ...overrides,
+  }
+}
