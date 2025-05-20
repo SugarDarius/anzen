33d0a6ad5fc86ccaeb4d4750f45b504fb9bc748a
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-05-20 19:35:15 +0200

    use good path

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 
 import './globals.css'
 
