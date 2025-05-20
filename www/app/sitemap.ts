cc8840d04376032941a12d631c98d2dedecee4e9
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-05-20 14:40:22 +0200

    add sitemap file

---
+++ www/app/sitemap.ts
@@ -0,0 +1,10 @@
+import type { MetadataRoute } from 'next'
+import { baseUrl } from '~/config/site'
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date().toISOString().split('T')[0],
+    },
+  ]
+}
