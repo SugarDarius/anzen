c0c920085ab2089793984fe08e4a7cb7dfe328a5
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-11-12 21:40:14 +0100

    rename server components

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/components/index.ts'],
+  entry: ['src/index.ts', 'src/server-components/index.ts'],
   dts: true,
   splitting: true,
   clean: true,
