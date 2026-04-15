21605f85502053304fd4540a07878194b04de1ed
Aurélien Dupays Dexemple <3740000+SugarDarius@users.noreply.github.com>
2026-04-15 10:39:42 +0200

    Update dependencies (#15)
    * update vitest
    
    * update node types
    
    * update prettier
    
    * update lucide react
    
    * update typescript eslint
    
    * update next
    
    * update react
    
    * update typescript
    
    * update release-it
    
    * update tsup config
    
    * keep release-it 19 for now

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/server-components/index.ts'],
-  dts: true,
+  dts: {
+    /**
+     * Ignore deprecations for TypeScript 6+
+     * 👉🏻 workaround to fix the build error with TypeScript 6+ when getting the deprecation error:
+     *   - "error TS5101: Option 'baseUrl' is deprecated and will stop functioning in TypeScript 7.0. Specify compilerOption '"ignoreDeprecations": "6.0"' to silence this error."
+     *   - https://github.com/egoist/tsup/issues/1388
+     */
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   splitting: true,
   clean: true,
   format: ['esm', 'cjs'],
