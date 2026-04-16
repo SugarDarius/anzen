078b97d60add3d851f464a58061ecb58bd087ce1
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-16 15:11:39 +0200

    update next config

--- www/next.config.ts
+++ www/next.config.ts
@@ -1,35 +1,45 @@
 import type { NextConfig } from 'next'
-import createMdx from '@next/mdx'
-import path from 'path'
+// import createMdx from '@next/mdx'
+// import path from 'path'
+
+// const nextConfig: NextConfig = {
+//   pageExtensions: ['ts', 'tsx', 'mdx'],
+// }
+
+// // Resolve plugin paths to absolute strings so they are serializable (Turbopack requires JSON-serializable loader options)
+// const rehypeRawStringMetaPath = path.resolve(process.cwd(), 'rehype-plugins/raw-string-meta.ts')
+// const rehypeFigureTitlePath = path.resolve(process.cwd(), 'rehype-plugins/figure-title.ts')
+
+// const withMdx = createMdx({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [],
+//     rehypePlugins: [
+//       'rehype-slug',
+//       rehypeRawStringMetaPath,
+//       ['rehype-pretty-code', { keepBackground: false, theme: 'vesper' }],
+//       rehypeFigureTitlePath,
+//       [
+//         'rehype-autolink-headings',
+//         {
+//           properties: {
+//             className: 'mdx-subheading-link',
+//           },
+//           behavior: 'wrap',
+//         },
+//       ],
+//     ],
+//   },
+// })
+
+// export default withMdx(nextConfig)
+
+import { createMDX } from 'fumadocs-mdx/next'
+
+const withMdx = createMDX()
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['ts', 'tsx', 'mdx'],
+  reactStrictMode: true,
 }
-
-// Resolve plugin paths to absolute strings so they are serializable (Turbopack requires JSON-serializable loader options)
-const rehypeRawStringMetaPath = path.resolve(process.cwd(), 'rehype-plugins/raw-string-meta.ts')
-const rehypeFigureTitlePath = path.resolve(process.cwd(), 'rehype-plugins/figure-title.ts')
-
-const withMdx = createMdx({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [
-      'rehype-slug',
-      rehypeRawStringMetaPath,
-      ['rehype-pretty-code', { keepBackground: false, theme: 'vesper' }],
-      rehypeFigureTitlePath,
-      [
-        'rehype-autolink-headings',
-        {
-          properties: {
-            className: 'mdx-subheading-link',
-          },
-          behavior: 'wrap',
-        },
-      ],
-    ],
-  },
-})
 
 export default withMdx(nextConfig)
