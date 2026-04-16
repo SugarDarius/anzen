cf7053030e8237999de3fe853bcc3b04a73979c5
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-16 17:59:04 +0200

    add retro grid effect

--- www/app/docs/[[...slug]]/page.tsx
+++ www/app/docs/[[...slug]]/page.tsx
@@ -10,6 +10,9 @@ import { createRelativeLink } from 'fumadocs-ui/mdx'
 
 import { source } from '~/lib/source'
 import { getMDXComponents } from '~/mdx-components'
+import { RetroGrid } from '~/components/retro-grid'
+import { PageActions } from '~/components/ai/page-actions'
+import { siteConfig } from '~/config/site'
 
 export async function generateStaticParams() {
   return source.generateParams()
@@ -42,17 +45,35 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
   const MDX = page.data.body
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription>{page.data.description}</DocsDescription>
-      <DocsBody>
-        <MDX
-          components={getMDXComponents({
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
-          })}
-        />
-      </DocsBody>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      breadcrumb={{ enabled: true }}
+      className='relative'
+    >
+      <div className='absolute top-0 w-full h-[84px]'>
+        <div className='absolute inset-0'>
+          <RetroGrid />
+        </div>
+      </div>
+      <div className='flex flex-col w-full z-1'>
+        <div className='flex items-start justify-between gap-4 mb-8'>
+          <DocsTitle>{page.data.title}</DocsTitle>
+          <PageActions
+            markdownUrl={`${page.url}.mdx`}
+            githubUrl={`${siteConfig.github.url}/blob/www/main/content/${page.url}.mdx`}
+          />
+        </div>
+        <DocsDescription>{page.data.description}</DocsDescription>
+        <DocsBody>
+          <MDX
+            components={getMDXComponents({
+              // this allows you to link to other pages with relative file paths
+              a: createRelativeLink(source, page),
+            })}
+          />
+        </DocsBody>
+      </div>
     </DocsPage>
   )
 }
