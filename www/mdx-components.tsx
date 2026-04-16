195317fb4d47d302a8d23dfc62fefea96d40a05f
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-16 15:36:32 +0200

    update mdx components

--- www/mdx-components.tsx
+++ www/mdx-components.tsx
@@ -1,183 +1,210 @@
+// import type { MDXComponents } from 'mdx/types'
+
+// import { cn } from '~/lib/utils'
+
+// import { Badge } from '~/components/ui/badge'
+// import { Tabs, TabsList, TabsTrigger, TabsContent } from '~/components/ui/tabs'
+
+// import { Announcement } from '~/components/content/announcement'
+// import { Callout } from '~/components/content/callout'
+// import { CodeBlockCommand } from '~/components/content/code-block-command'
+// import { Highlight } from '~/components/content/highlight'
+// import { WindowFrame } from '~/components/content/window-frame'
+
+// const components: MDXComponents = {
+//   Announcement,
+//   Callout,
+//   a: ({
+//     className,
+//     href,
+//     ...props
+//   }: React.HTMLAttributes<HTMLAnchorElement> & { href: string }) => {
+//     const isInternalLink = href.startsWith('/') || href.startsWith('#')
+//     return (
+//       <a
+//         className={cn(
+//           'font-medium underline underline-offset-4 [.mdx-subheading-link]:underline [.mdx-subheading-link]:decoration-dashed [.mdx-subheading-link]:decoration-muted [.mdx-subheading-link]:underline-offset-6 decoration-1 hover:[.mdx-subheading-link]:decoration-muted-foreground transition-all',
+//           className
+//         )}
+//         {...props}
+//         {...(!isInternalLink
+//           ? { target: '_blank', rel: 'noopener noreferrer' }
+//           : {})}
+//         href={href}
+//       />
+//     )
+//   },
+//   Badge,
+//   blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+//     <blockquote
+//       className={cn('mt-6 border-l-2 pl-6 italic', className)}
+//       {...props}
+//     />
+//   ),
+//   code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+//     <code
+//       className={cn(
+//         'relative rounded p-[0.2rem] font-mono text-sm bg-muted',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+//     <h1
+//       className={cn(
+//         'font-heading mt-2 scroll-m-20 text-4xl font-bold',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+//     <h2
+//       className={cn(
+//         'font-heading mt-12 scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+//     <h3
+//       className={cn(
+//         'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+//     <h4
+//       className={cn(
+//         'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+//     <h5
+//       className={cn(
+//         'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+//     <h6
+//       className={cn(
+//         'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
+//         className
+//       )}
+//       {...props}
+//     />
+//   ),
+//   Highlight,
+//   hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
+//     <hr className='my-4 md:my-8' {...props} />
+//   ),
+//   pre: ({
+//     children,
+//     className,
+//     __rawString__,
+//     __title__,
+//     ...props
+//   }: React.HTMLAttributes<HTMLPreElement> & {
+//     __rawString__?: string
+//     __title__?: string
+//   }) => {
+//     const isNPMCommand =
+//       __rawString__ &&
+//       (__rawString__.startsWith('npm i') ||
+//         __rawString__.startsWith('npm install'))
+
+//     if (isNPMCommand) {
+//       const npmCommand = __rawString__.trim()
+//       const yarnCommand = npmCommand.replace('npm i', 'yarn add')
+//       const pnpmCommand = npmCommand.replace('npm i', 'pnpm add')
+//       const bunCommand = npmCommand.replace('npm i', 'bun add')
+
+//       return (
+//         <CodeBlockCommand
+//           npmCommand={npmCommand}
+//           yarnCommand={yarnCommand}
+//           pnpmCommand={pnpmCommand}
+//           bunCommand={bunCommand}
+//         />
+//       )
+//     }
+//     return (
+//       <WindowFrame
+//         className='w-full mt-6 max-w-5xl mx-auto'
+//         title={__title__ ?? 'api/route.ts'}
+//         value={__rawString__}
+//       >
+//         <pre
+//           className={cn(
+//             'relative py-4 px-2 w-ful font-mono cursor-text outline-none',
+//             '[&_code]:bg-transparent!',
+//             className
+//           )}
+//           {...props}
+//         >
+//           {children}
+//         </pre>
+//       </WindowFrame>
+//     )
+//   },
+//   p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
+//     <p className={cn('leading-7 not-first:mt-4', className)} {...props} />
+//   ),
+//   ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+//     <ul className={cn('mt-6 ml-6 list-disc', className)} {...props} />
+//   ),
+//   ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+//     <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
+//   ),
+//   li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+//     <li className={cn('mt-2', className)} {...props} />
+//   ),
+//   Tabs,
+//   TabsList,
+//   TabsTrigger,
+//   TabsContent,
+// }
+
+// export function useMDXComponents(overrides: MDXComponents): MDXComponents {
+//   return {
+//     ...components,
+//     ...overrides,
+//   }
+// }
+
 import type { MDXComponents } from 'mdx/types'
+import defaultMdxComponents from 'fumadocs-ui/mdx'
+import * as TabsComponents from 'fumadocs-ui/components/tabs'
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock'
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom'
 
-import { cn } from '~/lib/utils'
-
-import { Badge } from '~/components/ui/badge'
-import { Tabs, TabsList, TabsTrigger, TabsContent } from '~/components/ui/tabs'
-
-import { Announcement } from '~/components/content/announcement'
-import { Callout } from '~/components/content/callout'
-import { CodeBlockCommand } from '~/components/content/code-block-command'
-import { Highlight } from '~/components/content/highlight'
-import { WindowFrame } from '~/components/content/window-frame'
-
-const components: MDXComponents = {
-  Announcement,
-  Callout,
-  a: ({
-    className,
-    href,
-    ...props
-  }: React.HTMLAttributes<HTMLAnchorElement> & { href: string }) => {
-    const isInternalLink = href.startsWith('/') || href.startsWith('#')
-    return (
-      <a
-        className={cn(
-          'font-medium underline underline-offset-4 [.mdx-subheading-link]:underline [.mdx-subheading-link]:decoration-dashed [.mdx-subheading-link]:decoration-muted [.mdx-subheading-link]:underline-offset-6 decoration-1 hover:[.mdx-subheading-link]:decoration-muted-foreground transition-all',
-          className
-        )}
-        {...props}
-        {...(!isInternalLink
-          ? { target: '_blank', rel: 'noopener noreferrer' }
-          : {})}
-        href={href}
-      />
-    )
-  },
-  Badge,
-  blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <blockquote
-      className={cn('mt-6 border-l-2 pl-6 italic', className)}
-      {...props}
-    />
-  ),
-  code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <code
-      className={cn(
-        'relative rounded p-[0.2rem] font-mono text-sm bg-muted',
-        className
-      )}
-      {...props}
-    />
-  ),
-  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h1
-      className={cn(
-        'font-heading mt-2 scroll-m-20 text-4xl font-bold',
-        className
-      )}
-      {...props}
-    />
-  ),
-  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h2
-      className={cn(
-        'font-heading mt-12 scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0',
-        className
-      )}
-      {...props}
-    />
-  ),
-  h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h3
-      className={cn(
-        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
-        className
-      )}
-      {...props}
-    />
-  ),
-  h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h4
-      className={cn(
-        'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
-        className
-      )}
-      {...props}
-    />
-  ),
-  h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h5
-      className={cn(
-        'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
-        className
-      )}
-      {...props}
-    />
-  ),
-  h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h6
-      className={cn(
-        'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
-        className
-      )}
-      {...props}
-    />
-  ),
-  Highlight,
-  hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
-    <hr className='my-4 md:my-8' {...props} />
-  ),
-  pre: ({
-    children,
-    className,
-    __rawString__,
-    __title__,
-    ...props
-  }: React.HTMLAttributes<HTMLPreElement> & {
-    __rawString__?: string
-    __title__?: string
-  }) => {
-    const isNPMCommand =
-      __rawString__ &&
-      (__rawString__.startsWith('npm i') ||
-        __rawString__.startsWith('npm install'))
-
-    if (isNPMCommand) {
-      const npmCommand = __rawString__.trim()
-      const yarnCommand = npmCommand.replace('npm i', 'yarn add')
-      const pnpmCommand = npmCommand.replace('npm i', 'pnpm add')
-      const bunCommand = npmCommand.replace('npm i', 'bun add')
-
-      return (
-        <CodeBlockCommand
-          npmCommand={npmCommand}
-          yarnCommand={yarnCommand}
-          pnpmCommand={pnpmCommand}
-          bunCommand={bunCommand}
-        />
-      )
-    }
-    return (
-      <WindowFrame
-        className='w-full mt-6 max-w-5xl mx-auto'
-        title={__title__ ?? 'api/route.ts'}
-        value={__rawString__}
-      >
-        <pre
-          className={cn(
-            'relative py-4 px-2 w-ful font-mono cursor-text outline-none',
-            '[&_code]:bg-transparent!',
-            className
-          )}
-          {...props}
-        >
-          {children}
-        </pre>
-      </WindowFrame>
-    )
-  },
-  p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
-    <p className={cn('leading-7 not-first:mt-4', className)} {...props} />
-  ),
-  ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className={cn('mt-6 ml-6 list-disc', className)} {...props} />
-  ),
-  ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
-    <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
-  ),
-  li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <li className={cn('mt-2', className)} {...props} />
-  ),
-  Tabs,
-  TabsList,
-  TabsTrigger,
-  TabsContent,
+export function getMDXComponents(components?: MDXComponents) {
+  return {
+    ...defaultMdxComponents,
+    ...TabsComponents,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    img: (props) => <ImageZoom {...(props as any)} />,
+    ...components,
+  } satisfies MDXComponents
 }
 
-export function useMDXComponents(overrides: MDXComponents): MDXComponents {
-  return {
-    ...components,
-    ...overrides,
-  }
+export const useMDXComponents = getMDXComponents
+
+declare global {
+  type MDXProvidedComponents = ReturnType<typeof getMDXComponents>
 }
