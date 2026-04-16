655beba3910c697a17c50529b4f5ad00dd5e04a2
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-16 15:50:52 +0200

    add docs layout

---
+++ www/app/docs/layout.tsx
@@ -0,0 +1,12 @@
+import { DocsLayout } from 'fumadocs-ui/layouts/docs'
+
+import { source } from '~/lib/source'
+import { baseOptions } from '~/lib/shared-layout'
+
+export default function Layout({ children }: LayoutProps<'/docs'>) {
+  return (
+    <DocsLayout {...baseOptions} tree={source.getPageTree()}>
+      {children}
+    </DocsLayout>
+  )
+}
