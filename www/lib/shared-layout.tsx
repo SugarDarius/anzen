ec1cda2b7b2b43db7085fe28f3b96e2dda7e9e96
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-17 10:56:01 +0200

    update header

--- www/lib/shared-layout.tsx
+++ www/lib/shared-layout.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+import { BoxIcon } from 'lucide-react'
 
 import { siteConfig } from '~/config/site'
 import { NPMIcon } from '~/components/icons/npm'
@@ -6,7 +7,12 @@ import { NPMIcon } from '~/components/icons/npm'
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: siteConfig.title,
+      title: (
+        <div className='flex items-center border bg-fd-background justify-center w-fit rounded-full gap-1 transition-all shadow-xs hover:bg-fd-accent px-2 py-1'>
+          <BoxIcon className='size-4' />
+          <span className='text-sm font-medium'>{siteConfig.title}</span>
+        </div>
+      ),
       url: '/',
     },
     githubUrl: siteConfig.github.url,
