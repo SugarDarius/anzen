99c3aef87fc6f90bd00a3d18c9c6fceda8bccb7b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-17 11:19:53 +0200

    add icon

--- www/app/docs/layout.tsx
+++ www/app/docs/layout.tsx
@@ -5,7 +5,27 @@ import { baseOptions } from '~/lib/shared-layout'
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout {...baseOptions()} tree={source.getPageTree()}>
+    <DocsLayout
+      {...baseOptions()}
+      tree={source.getPageTree()}
+      tabs={{
+        transform: (option, node) => {
+          const meta = source.getNodeMeta(node)
+          if (!meta || !node.icon) {
+            return null
+          }
+
+          return {
+            ...option,
+            icon: (
+              <div className='[&_svg]:size-full rounded-lg size-full text-(--color-fd-foreground) max-md:bg-(--color-fd-foreground)/10 max-md:border max-md:p-1.5'>
+                {node.icon}
+              </div>
+            ),
+          }
+        },
+      }}
+    >
       {children}
     </DocsLayout>
   )
