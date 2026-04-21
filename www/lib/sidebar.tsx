0b3f7e838d533d5bb070da1c5331ef7eb1234c3b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-04-21 12:33:27 +0200

    better badge and pulse

--- www/lib/sidebar.tsx
+++ www/lib/sidebar.tsx
@@ -21,10 +21,14 @@ export function addSidebarBadge({
               {node.name}
               <span
                 className={cn(
-                  'inline-flex ms-auto items-center justify-center rounded-md border px-2 py-0.5 text-xs font-semibold w-fit whitespace-nowrap shrink-0 transition-[color,box-shadow] overflow-hidden',
-                  'bg-fd-foreground/90 text-fd-background text-xs shadow-sm'
+                  'inline-flex ms-auto items-center justify-center rounded-full border border-border px-2 py-0.5 text-xs font-semibold w-fit whitespace-nowrap shrink-0 transition-[color,box-shadow] overflow-hidden',
+                  'bg-fd-secondary/30 text-fd-muted-foreground text-[10px] gap-1.5'
                 )}
               >
+                <span className='relative flex size-1.5'>
+                  <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-cyan-400 opacity-75'></span>
+                  <span className='relative inline-flex rounded-full size-1.5 bg-cyan-400'></span>
+                </span>
                 {badge}
               </span>
             </>
